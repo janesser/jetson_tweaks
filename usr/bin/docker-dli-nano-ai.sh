d41a30de842b1b4d9adc7ad644d61c5d150536cf
Jan Esser <jesser@gmx.de>
2021-06-09 15:44:19 +0200

    restart nvargus

--- usr/bin/docker-dli-nano-ai.sh
+++ usr/bin/docker-dli-nano-ai.sh
@@ -2,10 +2,16 @@
 
 VERSION=v2.0.1-r32.5.0
 
-docker run --runtime nvidia -it --rm \
-        --network host \
-        --volume ~/nvdli-data:/nvdli-nano/data \
-        --volume /tmp/argus_socket:/tmp/argus_socket \
+sudo systemctl restart nvargus-daemon 
+
+docker run \
+	--runtime nvidia \
+	-it --rm \
+	--network host \
+        --volume ~/jetson_tweaks/home/nvdli-data:/nvdli-nano/data \
+	--volume ~/jetson_tweaks/home/jupyter_notebooks:/nvdli-nano/jupyter_notebooks \
+	--volume /tmp/argus_socket:/tmp/argus_socket \
         --device /dev/video0 \
         --device /dev/video1 \
-        nvcr.io/nvidia/dli/dli-nano-ai:$VERSION
+        nvcr.io/nvidia/dli/dli-nano-ai:$VERSION \
+	$*
